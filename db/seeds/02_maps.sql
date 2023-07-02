dddc7443f0a3f461a3661da44b7d78a35fdfcdda
teksts <mark.egelhoff1@gmail.com>
2023-07-02 16:58:44 -0700

    Update map schema and seed to include lat, lon, and zoom values

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,14 +1,14 @@
-INSERT INTO maps (user_id, title, description, last_edit)
+INSERT INTO maps (user_id, latitude, longitude, zoom, title, description, last_edit)
 VALUES
-(1, 'Tall Buildings', 'A collection of tall buildings around the world', '2023-06-15 20:30:00'),
-(2, 'Awesome Trees', 'Beautiful and unique trees in different locations', '2023-06-20 12:45:00'),
-(3, 'Historical Landmarks', 'Famous historical landmarks and monuments', '2023-06-25 09:15:00'),
-(4, 'Stunning Beaches', 'Gorgeous beaches with crystal-clear water', '2023-06-10 15:20:00'),
-(5, 'Cozy Cafes', 'Charming cafes to relax and enjoy a cup of coffee', '2023-06-12 18:10:00'),
-(6, 'Hidden Waterfalls', 'Secret waterfalls nestled in remote locations', '2023-06-28 09:30:00'),
-(7, 'Colorful Street Art', 'Vibrant street art and graffiti from around the world', '2023-06-17 14:55:00'),
-(8, 'National Parks', 'Spectacular national parks and nature reserves', '2023-06-22 11:40:00'),
-(9, 'Charming Villages', 'Quaint and picturesque villages with rich history', '2023-06-24 16:25:00'),
-(10, 'Famous Bridges', 'Iconic bridges spanning rivers and connecting cities', '2023-06-13 19:50:00'),
-(1, 'My Houses', 'Places I have lived in the past', '2023-07-01 01:00:00'),
-(1, 'Cool Rocks', 'Wicked sick rocks my dude', '2023-06-21 12:00:00');
+(1, 40.6872, -74.0439, 50, 'Tall Buildings', 'A collection of tall buildings around the world', '2023-06-15 20:30:00'),
+(2, 48.8566, 2.3522, 50, 'Awesome Trees', 'Beautiful and unique trees in different locations', '2023-06-20 12:45:00'),
+(3, 48.8566, 2.3522, 50, 'Historical Landmarks', 'Famous historical landmarks and monuments', '2023-06-25 09:15:00'),
+(4, 25.0343, -77.3963, 50, 'Stunning Beaches', 'Gorgeous beaches with crystal-clear water', '2023-06-10 15:20:00'),
+(5, 37.7749, -122.4194, 50, 'Cozy Cafes', 'Charming cafes to relax and enjoy a cup of coffee', '2023-06-12 18:10:00'),
+(6, 40.6879, -74.0432, 50, 'Hidden Waterfalls', 'Secret waterfalls nestled in remote locations', '2023-06-28 09:30:00'),
+(7, 40.6887, -74.0456, 50, 'Colorful Street Art', 'Vibrant street art and graffiti from around the world', '2023-06-17 14:55:00'),
+(8, 40.6901, -74.0424, 50, 'National Parks', 'Spectacular national parks and nature reserves', '2023-06-22 11:40:00'),
+(9, 40.6922, -74.0428, 50, 'Charming Villages', 'Quaint and picturesque villages with rich history', '2023-06-24 16:25:00'),
+(10, 40.6929, -74.0446,50, 'Famous Bridges', 'Iconic bridges spanning rivers and connecting cities', '2023-06-13 19:50:00'),
+(1, 40.6930, -74.0455, 50, 'My Houses', 'Places I have lived in the past', '2023-07-01 01:00:00'),
+(1, 40.6875, -74.0443, 50, 'Cool Rocks', 'Wicked sick rocks my dude', '2023-06-21 12:00:00');
